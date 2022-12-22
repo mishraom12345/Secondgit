d83b3cd5193d95ecdb25dc36b7e1590c8ac78c3f
mishraom12345 <mishrahariom525@gmail.com>
2022-12-22 22:51:13 +0530

    task 13

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var item = document.getElementById('sita')
     var annu = document.getElementById('pawan')
-    
+    let radha = document.getElementsByClassName('edit')
     item.addEventListener('click',additem)
-    
+    annu.addEventListener('click',removeItem)
+    //radha.addEventListener('click',editItem)
 
     function additem(e){
         e.preventDefault();
@@ -23,13 +24,53 @@ var item = document.getElementById('sita')
         //create list
         let lst = document.createElement('li')
         lst.id = 'abc'
+        lst.className = 'asdf'
 
-        lst.appendChild(document.createTextNode(name1+''+emailId))
-        unorganised.appendChild(li)
+        lst.appendChild(document.createTextNode(name1+ ' '+emailId));
+        unorganised.appendChild(lst)
 
+        //create edit button
+        let edit1 = document.createElement('button');
+        edit1.className = 'edit'
         
+        edit1.appendChild(document.createTextNode('edit'))
+        lst.appendChild(edit1)
+
+        //create delete button
+        let deletebtn = document.createElement('button')
+        deletebtn.className = 'delete'
+        deletebtn.appendChild(document.createTextNode('delete'))
+        lst.appendChild(deletebtn)    
+
 
 
     }
 
-    
+    function removeItem(e){
+
+        if(e.target.classList.contains('delete')){
+
+        let lst = e.target.parentElement;
+        console.log(lst)
+        annu.removeChild(lst)
+        localStorage.removeItem(obj.email)
+
+        }
+         
+
+    }
+
+    // function editItem(e){
+    //     if(e.target.classList.contains('edit')){
+    //         let lst = e.target.parentElement;
+    //         console.log(lst)
+    //         annu.removeChild(lst)
+                
+
+    //         document.getElementById('sbi').value = e.target.parentElement.name
+
+    //         document.getElementById('cbi').value = e.target.parentElement.email
+            
+    //     }
+    // }
+
